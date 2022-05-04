f5605e769d7be8b89fa496a8d44118a3840c3238
qrouet <quentinrouet@hotmail.fr>
2022-05-04 10:07:57 +0200

    DataBinding

--- mod4databinding/src/main/java/com/eni/mod4databinding/MainActivity.kt
+++ mod4databinding/src/main/java/com/eni/mod4databinding/MainActivity.kt
@@ -2,10 +2,17 @@ package com.eni.mod4databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.eni.mod4databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(
+            this,R.layout.activity_main)
+        binding.u = User("Elon","Mousque",45,
+            "Pretoria","0321654987")
     }
 }
